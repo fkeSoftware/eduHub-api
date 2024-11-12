6fc5b92c759bbcf5357778918c4d8b565c1fa7e2
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-11-12 12:56:19 +0300

    Initial commit for EduHub project

---
+++ src/main/java/com/eduhub/edu_hub_api/EduHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.eduhub.edu_hub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduHubApiApplication.class, args);
+	}
+
+}
