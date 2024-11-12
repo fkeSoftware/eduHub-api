bef514d533aa379240f33b41c4e2f74c569577b2
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-11-12 21:02:20 +0300

    modified pom.xml (configured swagger version)

---
+++ src/main/java/com/eduhub/edu_hub_api/core/config/ApplicationConfig.java
@@ -0,0 +1,14 @@
+package com.eduhub.edu_hub_api.core.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
